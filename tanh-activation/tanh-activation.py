a533da132e2306e1e09ba5501427735bc698b44d
Kevin Johnson <kj89889@gmail.com>
2026-04-02 22:13:41 -0500

    Update solution for tanh-activation

--- tanh-activation/tanh-activation.py
+++ tanh-activation/tanh-activation.py
@@ -4,5 +4,7 @@ def tanh(x):
     """
     Implement Tanh activation function.
     """
-    return np.tanh(x)
+    tanh_fx = (np.exp(np.clip(x, -500,500)) - np.exp(-np.clip(x, -500,500))) / (np.exp(np.clip(x, -500,500)) + np.exp(-np.clip(x, -500,500)))
+    
+    return tanh_fx
     pass
