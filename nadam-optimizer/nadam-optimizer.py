a8e94923a8283ff5337214701573841da2337f85
Kevin Johnson <kj89889@gmail.com>
2026-04-04 22:34:08 -0500

    Update solution for nadam-optimizer

--- nadam-optimizer/nadam-optimizer.py
+++ nadam-optimizer/nadam-optimizer.py
@@ -9,8 +9,8 @@ def nadam_step(w, m, v, grad, lr=0.002, beta1=0.9, beta2=0.999, eps=1e-8):
     v = np.array(v)
     grad = np.array(grad)
     
-    first_moment = (beta1 * m) + ((1 - beta1) * grad)
-    second_moment = (beta2 * v) + ((1 - beta2) * np.square(grad))
-    nest_update = w - (lr * ((beta1 * first_moment) + (1 - beta1) * grad) / (np.sqrt(second_moment) + eps))
-    return (nest_update, first_moment, second_moment)
+    new_m = (beta1 * m) + ((1 - beta1) * grad)
+    new_v = (beta2 * v) + ((1 - beta2) * np.square(grad))
+    new_w = w - (lr * ((beta1 * new_m) + (1 - beta1) * grad) / (np.sqrt(new_v) + eps))
+    return (new_w, new_m, new_v)
     pass
